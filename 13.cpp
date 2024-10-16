ce870d8fe1c079fcb88f3443ec9f7238b8fb711b
MeowBuilder <tta122057@gmail.com>
2024-10-16 11:19:16 +0900

    1016

---
+++ 13.cpp
@@ -0,0 +1,314 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LINE_LENGTH 100
+using namespace std;
+
+typedef struct {
+	float x, y, z;
+}Vertex;
+
+typedef struct {
+	unsigned int v1, v2, v3;
+}Face;
+
+typedef struct {
+	Vertex* vertices;
+	size_t vertex_count;
+	Face* faces;
+	size_t face_count;
+}Model;
+
+void read_newline(char* str) {
+	char* pos;
+	if ((pos = strchr(str,'\n')) != NULL)
+	{
+		*pos = '\0';
+	}
+}
+
+void read_obj_file(const char* filename, Model* model) {
+	FILE* file;
+	fopen_s(&file, filename, "r");
+	if (!file) {
+		perror("Error opening file");
+		exit(EXIT_FAILURE);
+	}
+	char line[MAX_LINE_LENGTH];
+	model->vertex_count = 0;
+	model->face_count = 0;
+	while (fgets(line, sizeof(line), file)) {
+		read_newline(line);
+		if (line[0] == 'v' && line[1] == ' ')
+			model->vertex_count++;
+		else if (line[0] == 'f' && line[1] == ' ')
+			model->face_count++;
+	}
+	fseek(file, 0, SEEK_SET);
+	model->vertices = (Vertex*)malloc(model->vertex_count * sizeof(Vertex));
+	model->faces = (Face*)malloc(model->face_count * sizeof(Face));
+	size_t vertex_index = 0; size_t face_index = 0;
+	while (fgets(line, sizeof(line), file)) {
+		read_newline(line);
+		if (line[0] == 'v' && line[1] == ' ') {
+			int result = sscanf_s(line + 2, "%f %f %f", &model->vertices[vertex_index].x,
+				&model->vertices[vertex_index].y,
+				&model->vertices[vertex_index].z);
+			vertex_index++;
+		}
+		else if (line[0] == 'f' && line[1] == ' ') {
+			unsigned int v1, v2, v3;
+			int result = sscanf_s(line + 2, "%u %u %u", &v1, &v2, &v3);
+			model->faces[face_index].v1 = v1 - 1; // OBJ indices start at 1
+			model->faces[face_index].v2 = v2 - 1;
+			model->faces[face_index].v3 = v3 - 1;
+			face_index++;
+		}
+	}
+	fclose(file);
+}
+
+//--- 필요한 헤더파일 선언
+#include <gl/glew.h>
+#include <gl/freeglut.h>
+#include <gl/freeglut_ext.h>
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+char* filetobuf(const char* file)
+{
+	FILE* fptr;
+	long length;
+	char* buf;
+	fptr = fopen(file, "rb"); // Open file for reading
+	if (!fptr) // Return NULL on failure
+		return NULL;
+	fseek(fptr, 0, SEEK_END); // Seek to the end of the file
+	length = ftell(fptr); // Find out how many bytes into the file we are
+	buf = (char*)malloc(length + 1); // Allocate a buffer for the entire length of the file and a null terminator
+	fseek(fptr, 0, SEEK_SET); // Go back to the beginning of the file
+	fread(buf, length, 1, fptr); // Read the contents of the file in to the buffer
+	fclose(fptr); // Close the file
+	buf[length] = 0; // Null terminator
+	return buf; // Return the buffer
+}
+
+GLclampf convert_to_clampf_X(int x, int w) {
+	return (x - (w / 2.0f)) / (w / 2.0f);
+}
+GLclampf convert_to_clampf_Y(int y, int h) {
+	return (h / 2.0f - y) / (h / 2.0f);
+}
+
+//--- 사용자 정의 함수
+void make_vertexShaders();
+void make_fragmentShaders();
+GLuint make_shaderProgram();
+GLvoid drawScene();
+GLvoid Reshape(int w, int h);
+GLvoid Keyboard(unsigned char key, int x, int y);
+void TimerFunction(int value);
+void Mouse(int button, int state, int x, int y);
+
+//--- 필요한 변수 선언
+GLint width, height;
+GLuint shaderProgram; //--- 세이더 프로그램 이름
+GLuint vertexShader; //--- 버텍스 세이더 객체
+GLuint fragmentShader; //--- 프래그먼트 세이더 객체
+GLuint VAO, VBO, Line_VBO;
+Model* cur_model = (Model*)malloc(sizeof(Model*));
+
+GLvoid InitBuffer()
+{
+	//--- VAO와 VBO 객체 생성
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &Line_VBO);
+}
+
+//--- 메인 함수
+void main(int argc, char** argv) //--- 윈도우 출력하고 콜백함수 설정
+{
+	width = 800;
+	height = 600;
+	//--- 윈도우 생성하기
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(width, height);
+	glutCreateWindow("Example1");
+
+	//--- GLEW 초기화하기
+	glewExperimental = GL_TRUE;
+	glewInit();
+
+	//--- VAO/VBO 버퍼 초기화
+	shaderProgram = make_shaderProgram();
+	InitBuffer();
+
+	glEnable(GL_DEPTH_TEST);
+
+	read_obj_file("object.obj", cur_model);
+	glutTimerFunc(10, TimerFunction, 0);
+
+	//--- 세이더 프로그램 만들기
+	glutDisplayFunc(drawScene);
+	glutReshapeFunc(Reshape);
+	glutKeyboardFunc(Keyboard);
+	glutMouseFunc(Mouse);
+	glutMainLoop();
+}
+
+void make_vertexShaders()
+{
+	GLchar* vertexSource;
+
+	vertexSource = filetobuf("vertex.glsl");
+
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexSource, NULL);
+	glCompileShader(vertexShader);
+
+	GLint result;
+	GLchar errorLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &result);
+	if (!result)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, errorLog);
+		std::cerr << "ERROR: vertex shader 컴파일 실패\n" << errorLog << std::endl;
+		return;
+	}
+}
+
+void make_fragmentShaders()
+{
+	GLchar* fragmentSource;
+	//--- 프래그먼트 세이더 읽어 저장하고 컴파일하기
+	fragmentSource = filetobuf("fragment.glsl"); // 프래그세이더 읽어오기
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+	glCompileShader(fragmentShader);
+
+	GLint result;
+	GLchar errorLog[512];
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &result);
+	if (!result)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, errorLog);
+		std::cerr << "ERROR: frag_shader 컴파일 실패\n" << errorLog << std::endl;
+		return;
+	}
+}
+
+GLuint make_shaderProgram()
+{
+	make_vertexShaders();
+	make_fragmentShaders();
+	GLuint shaderID;
+	shaderID = glCreateProgram(); //--- 세이더 프로그램 만들기
+	glAttachShader(shaderID, vertexShader); //--- 세이더 프로그램에 버텍스 세이더 붙이기
+	glAttachShader(shaderID, fragmentShader); //--- 세이더 프로그램에 프래그먼트 세이더 붙이기
+	glLinkProgram(shaderID); //--- 세이더 프로그램 링크하기
+
+	glDeleteShader(vertexShader); //--- 세이더 객체를 세이더 프로그램에 링크했음으로, 세이더 객체 자체는 삭제 가능
+	glDeleteShader(fragmentShader);
+
+	GLint result;
+	GLchar errorLog[512];
+	glGetProgramiv(shaderID, GL_LINK_STATUS, &result); // ---세이더가 잘 연결되었는지 체크하기
+	if (!result) {
+		glGetProgramInfoLog(shaderID, 512, NULL, errorLog);
+		std::cerr << "ERROR: shader program 연결 실패\n" << errorLog << std::endl;
+		return false;
+	}
+	glUseProgram(shaderID); //--- 만들어진 세이더 프로그램 사용하기
+
+	return shaderID;
+}
+
+
+GLvoid drawScene() //--- 콜백 함수: 그리기 콜백 함수
+{
+	GLfloat rColor, gColor, bColor;
+	rColor = 1.0;
+	gColor = 1.0;
+	bColor = 1.0;
+	glClearColor(rColor, gColor, bColor, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glUseProgram(shaderProgram);
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, cur_model->vertex_count * sizeof(float), cur_model->vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0); // 위치
+	glEnableVertexAttribArray(0);
+
+	vector<float> color = {
+		1.0f, 0.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+		0.0f, 0.0f, 1.0f,
+		1.0f, 1.0f, 0.0f,
+		1.0f, 0.0f, 1.0f,
+		0.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		0.0f, 0.0f, 0.0f
+	};
+
+	glBufferData(GL_ARRAY_BUFFER, color.size() * sizeof(float), color.data(), GL_STATIC_DRAW);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0); // 색상
+	glEnableVertexAttribArray(1);
+
+	glDrawArrays(GL_FILL, 0, cur_model->vertex_count);
+
+
+	glutSwapBuffers(); // 화면에 출력하기
+}
+
+//--- 다시그리기 콜백 함수
+GLvoid Reshape(int w, int h) //--- 콜백 함수: 다시 그리기 콜백 함수
+{
+	glViewport(0, 0, w, h);
+}
+
+GLvoid Keyboard(unsigned char key, int x, int y) {
+	switch (key) {
+
+	case 'q':
+		glutLeaveMainLoop();
+		break;
+	}
+	glutPostRedisplay();
+}
+
+void Mouse(int button, int state, int x, int y)
+{
+	GLclampf cx = convert_to_clampf_X(x, 800);
+	GLclampf cy = convert_to_clampf_Y(y, 600);
+	if (button == GLUT_LEFT_BUTTON) {
+		if (state == GLUT_DOWN)
+		{
+
+		}
+		else if (state == GLUT_UP)
+		{
+
+		}
+	}
+
+	glutPostRedisplay();
+}
+
+
+void TimerFunction(int value) {
+
+	glutTimerFunc(10, TimerFunction, 0);
+	glutPostRedisplay();
+}
